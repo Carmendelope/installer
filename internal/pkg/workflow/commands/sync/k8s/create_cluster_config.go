df8f8b9a4bcb5fb5827744d8de229df37cb0cedb
Rodrigo Núñez <rnunez@daisho.group>
2018-12-17 10:23:12 +0100

    Fixed unit tests

--- internal/pkg/workflow/commands/sync/k8s/create_cluster_config.go
+++ internal/pkg/workflow/commands/sync/k8s/create_cluster_config.go
@@ -7,37 +7,49 @@ package k8s
 import (
 	"encoding/json"
 	"fmt"
+	"strings"
+
 	"github.com/nalej/derrors"
 	"github.com/nalej/installer/internal/pkg/errors"
 	"github.com/nalej/installer/internal/pkg/workflow/entities"
 	"github.com/rs/zerolog/log"
 	"k8s.io/api/core/v1"
 	v12 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"strings"
 )
 
 type CreateClusterConfig struct {
 	Kubernetes
-	OrganizationID       string `json:"organization_id"`
-	ClusterID            string `json:"cluster_id"`
-	ManagementPublicHost string `json:"management_public_host"`
-	ManagementPublicPort string `json:"management_public_port"`
+	OrganizationID        string `json:"organization_id"`
+	ClusterID             string `json:"cluster_id"`
+	ManagementPublicHost  string `json:"management_public_host"`
+	ManagementPublicPort  string `json:"management_public_port"`
 	ClusterPublicHostname string `json:"cluster_public_hostname"`
+	DNSPublicHost         string `json:"dns_public_host"`
+	DNSPublicPort         string `json:"dns_public_port"`
+	PlatformType          string `json:"platform_type"`
 }
 
 func NewCreateClusterConfig(
 	kubeConfigPath string,
 	organizationID string, clusterID string,
-	managementPublicHost string, managementPublicPort string) *CreateClusterConfig {
+	managementPublicHost string, managementPublicPort string,
+	clusterPublicHostname string,
+	dnsPublicHost string, dnsPublicPort string,
+	platformType string,
+) *CreateClusterConfig {
 	return &CreateClusterConfig{
 		Kubernetes: Kubernetes{
 			GenericSyncCommand: *entities.NewSyncCommand(entities.CreateClusterConfig),
 			KubeConfigPath:     kubeConfigPath,
 		},
-		OrganizationID:       organizationID,
-		ClusterID:            clusterID,
-		ManagementPublicHost: managementPublicHost,
-		ManagementPublicPort: managementPublicPort,
+		OrganizationID:        organizationID,
+		ClusterID:             clusterID,
+		ManagementPublicHost:  managementPublicHost,
+		ManagementPublicPort:  managementPublicPort,
+		ClusterPublicHostname: clusterPublicHostname,
+		DNSPublicHost:         dnsPublicHost,
+		DNSPublicPort:         dnsPublicPort,
+		PlatformType:          platformType,
 	}
 }
 
@@ -51,8 +63,6 @@ func NewCreateClusterConfigFromJSON(raw []byte) (*entities.Command, derrors.Erro
 	return &r, nil
 }
 
-
-
 func (ccc *CreateClusterConfig) Run(workflowID string) (*entities.CommandResult, derrors.Error) {
 	connectErr := ccc.Connect()
 	if connectErr != nil {
@@ -60,7 +70,12 @@ func (ccc *CreateClusterConfig) Run(workflowID string) (*entities.CommandResult,
 	}
 
 	mgntIPs, rErr := ccc.ResolveIP(ccc.ManagementPublicHost)
-	if rErr != nil{
+	if rErr != nil {
+		return nil, rErr
+	}
+
+	dnsIPs, rErr := ccc.ResolveIP(ccc.DNSPublicHost)
+	if rErr != nil {
 		return nil, rErr
 	}
 
@@ -80,14 +95,17 @@ func (ccc *CreateClusterConfig) Run(workflowID string) (*entities.CommandResult,
 			Labels:    map[string]string{"cluster": "application"},
 		},
 		Data: map[string]string{
-			"organization_id":        ccc.OrganizationID,
-			"cluster_id":             ccc.ClusterID,
-			"management_public_host": ccc.ManagementPublicHost,
-			"management_public_ip": strings.Join(mgntIPs, ","),
-			"management_public_port": ccc.ManagementPublicPort,
+			"organization_id":         ccc.OrganizationID,
+			"cluster_id":              ccc.ClusterID,
+			"management_public_host":  ccc.ManagementPublicHost,
+			"management_public_ip":    strings.Join(mgntIPs, ","),
+			"management_public_port":  ccc.ManagementPublicPort,
 			"cluster_public_hostname": ccc.ClusterPublicHostname,
-			"cluster_api_hostname": fmt.Sprintf("cluster.%s", ccc.ManagementPublicHost),
-			"login_api_hostname": fmt.Sprintf("login.%s", ccc.ManagementPublicHost),
+			"cluster_api_hostname":    fmt.Sprintf("cluster.%s", ccc.ManagementPublicHost),
+			"login_api_hostname":      fmt.Sprintf("login.%s", ccc.ManagementPublicHost),
+			"dns_public_ips":          strings.Join(dnsIPs, ","),
+			"dns_public_port":         ccc.DNSPublicPort,
+			"platform_type":           ccc.PlatformType,
 		},
 	}
 
@@ -108,12 +126,12 @@ func (ccc *CreateClusterConfig) String() string {
 
 func (ccc *CreateClusterConfig) PrettyPrint(indentation int) string {
 	simpleIden := strings.Repeat(" ", indentation)
-	entrySep := simpleIden +  "  "
-	msg := fmt.Sprintf("\n%sConfig:\n%sManagementPublicHost: %s\n%sClusterPubliHostname: %s",
+	entrySep := simpleIden + "  "
+	msg := fmt.Sprintf("\n%sConfig:\n%sManagementPublicHost: %s\n%sClusterPublicHostname: %s",
 		entrySep,
 		entrySep, ccc.ManagementPublicHost,
 		entrySep, ccc.ClusterPublicHostname,
-		)
+	)
 	return strings.Repeat(" ", indentation) + ccc.String() + msg
 }
 
